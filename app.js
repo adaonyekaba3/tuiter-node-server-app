2e57fe5362282cd70f641837523d2b70ec01a5b3
Ada Onyekaba <62957076+adaonyekaba3@users.noreply.github.com>
2023-04-09 14:39:57 -0400

    first commit pls work

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-import express from 'express'
+import express from 'express';
+import mongoose from "mongoose";
+import cors from 'cors'
+import dotenv from "dotenv";
+
+// Import controllers
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+
+dotenv.config();
+const app = express()   // create an instance of the express library and assign it to local constant app
+/*
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+*/
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+app.use(cors())
+app.use(express.json());
+TuitsController(app);
+HelloController(app)
+UserController(app)
+app.listen(process.env.PORT || 4000)
+
+/*import express from 'express'
 import HelloController
     from "./controllers/hello-controller.js"
 import UserController
@@ -13,4 +39,4 @@ app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000);*/
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,3 +1,46 @@
+import * as tuitsDao from '../../tuits/tuits-dao.js'
+
+const createTuit = async (req, res) => {
+    const newTuit = req.body;
+    newTuit.likes = 0;
+    newTuit.dislikes = 0;
+    newTuit.liked = false;
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
+}
+
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits()
+    res.json(tuits);
+}
+
+const findTuitById = async (req, res) => {
+    const tuit = await tuitsDao.findTuitById(req.params.tid)
+    res.json(tuit);
+}
+
+const updateTuit = async (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const status = await tuitsDao.updateTuit(tuitdIdToUpdate,updates);
+    res.json(status);
+}
+
+const deleteTuit = async (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+    res.json(status);
+}
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.get('/api/tuits/:tid', findTuitById);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
+
+/*
 import posts from "./tuits.js";
 let tuits = posts;
 
@@ -36,3 +79,4 @@ export default (app) => {
     app.delete('/api/tuits/:tid', deleteTuit);
 }
 
+*/
